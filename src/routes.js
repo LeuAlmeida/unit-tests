b2f668bd31fc6b8558d3157cb038f2990dc0a940
LeuAlmeida <leo@webid.net.br>
2020-02-04 17:09:48 -0300

    refactor: Importing controllers

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
 
-routes.post('/users', (req, res) => res.json({ id: 1 }));
+routes.post('/users', UserController.store);
 
 export default routes;
