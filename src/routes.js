8ca778b823844509819a3e7524ec72b8cb164318
LeuAlmeida <leo@webid.net.br>
2020-02-04 16:39:50 -0300

    feat: Create /users route POST

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,6 @@ const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
 
+routes.post('/users', (req, res) => res.json({ id: 1 }));
+
 export default routes;
